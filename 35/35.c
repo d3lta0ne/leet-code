18da990e52066ec3df7977537ff25c799ac735b2
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2024-09-30 19:19:28 -0400

    solved: 35 in c

--- 35/35.c
+++ 35/35.c
@@ -1,26 +1,37 @@
+/***
+ * 35. Search Insert Position
+ * Author: D3lta0ne
+ * Published: 9/30/24
+ *
+ * Description:
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ * You must write an algorithm with O(log n) runtime complexity.
+ */
+
 int searchInsert(int *nums, int numsSize, int target)
 {
+    if (!nums || numsSize == 0)
+    {
+        return 0;
+    }
+
     int i = 0;
     int j = numsSize - 1;
-
     while (i <= j)
     {
-        int mid = i + (j - i) / 2; // Calculate the midpoint
-
-        if (nums[mid] == target)
+        if (target == nums[i])
         {
-            return mid; // Target found, return its index
+            return i;
         }
-        else if (nums[mid] < target)
+        else if (target < nums[i / 2])
         {
-            i = mid + 1; // Narrow search to the right half
+            j = (j / 2) + 1;
         }
         else
         {
-            j = mid - 1; // Narrow search to the left half
+            i = (j / 2) + 1;
         }
     }
-
-    // If target is not found, i is the insertion point
     return i;
 }
