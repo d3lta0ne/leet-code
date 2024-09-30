b3779599ca5d282e167186982f0fb105c8988908
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2024-09-30 19:07:17 -0400

    started: 35 in c

---
+++ 35/35.c
@@ -0,0 +1,14 @@
+/***
+ * 35. Search Insert Position
+ * Author: D3lta0ne
+ * Published: 9/30/24
+ *
+ * Description:
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ * You must write an algorithm with O(log n) runtime complexity.
+ */
+
+int searchInsert(int *nums, int numsSize, int target)
+{
+}
