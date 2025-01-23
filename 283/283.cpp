5e4987ac447e7924d57be974d3bda61c66b4ce29
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2025-01-23 18:18:39 -0500

    ac: solved in cpp

--- 283/283.cpp
+++ 283/283.cpp
@@ -1,13 +1,14 @@
+#include <vector>
 class Solution
 {
 public:
-    void moveZeroes(vector<int> &nums)
+    void moveZeroes(std::vector<int> &nums)
     {
         for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++)
         {
             if (nums[cur] != 0)
             {
-                swap(nums[lastNonZeroFoundAt++], nums[cur]);
+                std::swap(nums[lastNonZeroFoundAt++], nums[cur]);
             }
         }
     }
