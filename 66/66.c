cd51fe86c0c629729da8589193479247378b1377
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2025-01-23 18:51:52 -0500

    ac: 66 in c

--- 66/66.c
+++ 66/66.c
@@ -2,42 +2,31 @@
  * /***
  * 66. Plus One
  * Author: D3lta0ne
- * Published: 10/1/24
+ * Published: 1/23/25
  *
  * Description:
  * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
  * Increment the large integer by one and return the resulting array of digits.
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* plusOne(int* digits, int digitsSize, int* returnSize) {
-    // Allocate space for the new digits array
-    int* result = (int*)malloc((digitsSize + 1) * sizeof(int));
-    int carry = 1; // We start with a carry of 1 since we're adding one
-
-    // Iterate from the last digit to the first
-    for (int i = digitsSize - 1; i >= 0; i--) {
-        int sum = digits[i] + carry;
-        if (sum >= 10) {
-            result[i + 1] = sum % 10; // Store the last digit
-            carry = 1; // Carry remains 1
-        } else {
-            result[i] = sum; // No carry needed
-            carry = 0; // No carry going forward
-        }
-    }
-
-    // If there's still a carry after the most significant digit
-    if (carry) {
-        result[0] = 1; // The new most significant digit is 1
-        *returnSize = digitsSize + 1; // New size
-    }
-    else
+int *plusOne(int *digits, int digitsSize, int *returnSize)
+{
+    int i = digitsSize - 1;
+    while (i >= 0 && digits[i] == 9)
     {
-        for (int i = 0; i < digitsSize-1; i++) {
-            result[i] = result[i + 1];
-        }
-        *returnSize = digitsSize; // Old size
+        digits[i] = 0;
+        i--;
     }
 
-    return result;
+    if (i == -1)
+    {
+        *returnSize = digitsSize + 1;
+        int *res = (int *)calloc((*returnSize) * sizeof(int), sizeof(int));
+        res[0] = 1;
+        return res;
+    }
+
+    digits[i]++;
+    *returnSize = digitsSize;
+    return digits;
 }
