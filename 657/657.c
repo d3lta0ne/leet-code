b9dcf0b9cf88e1c7176b352a4bba4cee261bbf54
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2025-01-24 16:22:40 -0500

    ac: 657 in c

---
+++ 657/657.c
@@ -0,0 +1,32 @@
+bool judgeCircle(char *moves)
+{
+    int x, y, i;
+
+    i = 0;
+
+    x = 0;
+    y = 0;
+
+    while (moves[i])
+    {
+        if (moves[i] == 'U')
+        {
+            y++;
+        }
+        else if (moves[i] == 'D')
+        {
+            y--;
+        }
+        else if (moves[i] == 'L')
+        {
+            x--;
+        }
+        else if (moves[i] == 'R')
+        {
+            x++;
+        }
+        i++;
+    }
+
+    return x == 0 && y == 0;
+}
