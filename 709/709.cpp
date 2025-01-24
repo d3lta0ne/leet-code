a2a425aee16fa62eeeec3f1ed9d419db4b18c65a
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2025-01-24 14:42:27 -0500

    ac: 709 in cpp

--- 709/709.cpp
+++ 709/709.cpp
@@ -1,7 +1,13 @@
+#include <string>
 class Solution
 {
 public:
-    string toLowerCase(string s)
+    std::string toLowerCase(std::string s)
     {
+        for (int i = 0; i < s.size(); ++i)
+        {
+            s[i] = tolower(s[i]);
+        }
+        return s;
     }
 };
