0b854f09d053521ce333310da96af3da6d7c32da
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2025-01-24 16:08:47 -0500

    ac: 1st attempt w/ array

--- 657/657.cpp
+++ 657/657.cpp
@@ -1,7 +1,40 @@
 class Solution
 {
+
 public:
     bool judgeCircle(string moves)
     {
+        int chk[2]{};
+        for (char x : moves)
+        {
+            if (x == 'R')
+            {
+                chk[0]++;
+            }
+            else if (x == 'L')
+            {
+                chk[0]--;
+            }
+            else if (x == 'U')
+            {
+                chk[1]++;
+            }
+            else if (x == 'D')
+            {
+                chk[1]--;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        for (int ck : chk)
+        {
+            if (ck != 0)
+                return false;
+        }
+
+        return true;
     }
 };
