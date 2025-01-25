3413b22b16b918b4c4332dc30a0aa6a204534a9b
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2025-01-24 23:17:17 -0500

    ac: 1672 in cpp w/ loops

--- 1672/1672.cpp
+++ 1672/1672.cpp
@@ -7,10 +7,14 @@ using namespace std;
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int max_wealth = numeric_limits<int>::min();
-        for (vector<int> customer : accounts) {
-            int current_wealth = accumulate(customer.begin(), customer.end(), 0);
-            max_wealth = max(max_wealth, current_wealth); 
+        int max_wealth = -1;
+        for (auto customer : accounts){
+            int cur_wealth = 0;
+            for(auto bank : customer){
+                cur_wealth+=bank;
+            }
+
+            if(cur_wealth > max_wealth) max_wealth=cur_wealth;
         }
 
         return max_wealth;
