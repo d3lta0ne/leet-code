6bd4a7cbd49d57c6b06ccabd08fe49d3c7369edb
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2025-01-25 17:14:17 -0500

    ac: solved tracking visted nodes w/ cpp

---
+++ 54/54.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> spiralOrder(vector<vector<int>> &matrix)
+    {
+        const int VISITED = 101;
+        int rows = matrix.size(), columns = matrix[0].size();
+        // Four directions that we will move: right, down, left, up.
+        vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        // Initial direction: moving right.
+        int currentDirection = 0;
+        // The number of times we change the direction.
+        int changeDirection = 0;
+        // Current place that we are at is (row, col).
+        // row is the row index; col is the column index.
+        int row = 0, col = 0;
+        // Store the first element and mark it as visited.
+        vector<int> result = {matrix[0][0]};
+        matrix[0][0] = VISITED;
+        while (changeDirection < 2)
+        {
+            while (0 <= row + directions[currentDirection][0] &&
+                   row + directions[currentDirection][0] < rows &&
+                   0 <= col + directions[currentDirection][1] &&
+                   col + directions[currentDirection][1] < columns &&
+                   matrix[row + directions[currentDirection][0]]
+                         [col + directions[currentDirection][1]] != VISITED)
+            {
+                // Reset this to 0 since we did not break and change the
+                // direction.
+                changeDirection = 0;
+                // Calculate the next place that we will move to.
+                row += directions[currentDirection][0];
+                col += directions[currentDirection][1];
+                result.push_back(matrix[row][col]);
+                matrix[row][col] = VISITED;
+            }
+            // Change our direction.
+            currentDirection = (currentDirection + 1) % 4;
+            // Increment change_direction because we changed our direction.
+            changeDirection++;
+        }
+        return result;
+    }
+};
