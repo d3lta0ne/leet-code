41803f754b52bb2051ac9ee99f02fadf82ff977f
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2025-01-25 15:05:42 -0500

    update git

--- 1275/1275_faster.cpp
+++ 1275/1275_faster.cpp
@@ -1,20 +1,5 @@
 /*
-Algorithm
-
-    We use two lists, rows and cols of size n, to represent each row and column. We also use two numbers, diag and anti_diag, to represent the diagonal value and anti-diagonal value, respectively.
-
-    Set the value of player A as 1 and the value of player B as -1.
-
-    For each new move [row, col], add the player's value to rows[row] and cols[col]. If [row, col] is on the diagonal or anti-diagonal, then add the player's value to diag or anti_diag as well.
-
-    Then, check if this move meets any winning condition:
-        Check if all cells in the current row contain characters from this player. To do so, we just need to check if the absolute value of rows[row] equals n.
-        Check if all cells in the current column contain characters from this player. To do so, we just need to check if the absolute value of cols[col] equals n.
-        Check if this move is on the diagonal; that is if row equals col. If so, check if the absolute value of diag equals n.
-        Check if this move is on the anti-diagonal; that is if row + col equals n - 1. If so, check if the absolute value of anti_diag equals n.
-
-    If there is no winner after all of the moves have been played, we will check if the entire board is filled. If so, return "Draw", otherwise return "Pending", meaning the game is still on. To determine if the entire board is filled, check if the length of moves equals the number of cells on the n by n board.
-
+ 
 */
 
 #include <vector>
