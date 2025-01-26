a2ac33a831dcccdf73ba44512d8bea0662c50d02
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2025-01-25 20:35:23 -0500

    ac: 1491 in cpp using built-ins

--- 1491/1491.cpp
+++ 1491/1491.cpp
@@ -1,36 +1,17 @@
-#include <limits>
-#include <vector>
-
-using namespace std;
-
 class Solution
 {
 public:
     double average(vector<int> &salary)
     {
-        int min = numeric_limits<int>::max();
-        int max = numeric_limits<int>::min();
-        double count = 0.0;
+        int minSalary = *min_element(salary.begin(), salary.end());
+        int maxSalary = *max_element(salary.begin(), salary.end());
+        double sum = accumulate(salary.begin(), salary.end(), 0);
 
-        for (int employee : salary)
+        if (minSalary == maxSalary)
         {
-            count += employee;
-            if (employee > max)
-                max = employee;
-            if (employee < min)
-                min = employee;
+            return 0;
         }
 
-        // what if the min and the max salary at the same?
-        if (min == max)
-        {
-            count -= min;
-            return count / (salary.size() - 1);
-        }
-        else
-        {
-            count -= (min + max);
-            return count / (salary.size() - 2);
-        }
+        return (sum - minSalary - maxSalary) / double(salary.size() - 2);
     }
 };
