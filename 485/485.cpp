b80071b0eee357f4dba3f00f392000da03792254
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2025-01-25 22:01:08 -0500

    ac: 485 in cpp

--- 485/485.cpp
+++ 485/485.cpp
@@ -1,7 +1,25 @@
+#include <vector>
+
 class Solution
 {
 public:
-    int findMaxConsecutiveOnes(vector<int> &nums)
+    int findMaxConsecutiveOnes(std::vector<int> &nums)
     {
+        int max = 0;
+        int ctr = 0;
+        for (int n : nums)
+        {
+            if (n == 1)
+            {
+                ctr++;
+                if (ctr > max)
+                    max = ctr;
+            }
+            else
+            {
+                ctr = 0;
+            }
+        }
+        return max;
     }
 };
